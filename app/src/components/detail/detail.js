60eded2e80e3345076b5efde1a1c37007a0ca1d3
elhendricks <elhendricks@gmail.com>
2016-12-12 15:42:59 -0800

    changed detail background color

--- app/src/components/detail/detail.js
+++ app/src/components/detail/detail.js
@@ -1,8 +1,14 @@
 import template from './detail.html';
+import styles from './detail.scss';
 
 export default {
     template,
     bindings: {
         image: '='
-    }
+    }, 
+    controller
 };
+
+function controller() {
+    this.styles = styles;
+}
